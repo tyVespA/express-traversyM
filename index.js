f0849cb0feda7fc7ce6cc360fa94f8e35a2a6a1b
tyVespA <migliavacca.marco08@gmail.com>
2023-12-13 07:03:56 +0100

    Create base server

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 // in deployment the PORT number will be in an enviroment variable
 const PORT = process.env.PORT || 5000;
 
+const logger = require("./middleware/logger");
+const members = require("./Members");
+
+app.use(logger);
+
+// Set static folder
+// all files in public folder will be available
+// ex. about.hmtl will be available at /about.html
+// app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-  res.send("ABA");
+  // from the current directory, go in a folder called public, then go in a file colled index.html
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Get all members
+app.get("/api/members", (req, res) => {
+  // .json simply sends back the json
+  res.json(members);
+});
+
+// Get single member
+app.get("/api/members/:id", (req, res) => {
+  const memberId = req.params.id;
+  if (memberId > members.length - 1) {
+    res
+      .status(400)
+      .json({ msg: `Member with the id of ${memberId} not found` });
+  } else {
+    res.json(members[memberId]);
+  }
+  // res.json(members.filter((member) => member.id === parseInt(req.params.id)));
 });
 
 app.listen(PORT, () => {
