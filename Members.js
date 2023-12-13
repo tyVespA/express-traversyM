74f91d169d32a837457965efaf5adb96ef54e07a
tyVespA <migliavacca.marco08@gmail.com>
2023-12-13 07:03:58 +0100

    Create Members.js

---
+++ Members.js
@@ -0,0 +1,37 @@
+const members = [
+  {
+    name: "John Doe",
+    age: 30,
+    city: "Example City",
+    isStudent: false,
+    grades: [95, 87, 92],
+    address: {
+      street: "123 Main Street",
+      zipCode: "12345",
+    },
+  },
+  {
+    name: "Jane Smith",
+    age: 25,
+    city: "Another City",
+    isStudent: true,
+    grades: [88, 95, 89],
+    address: {
+      street: "456 Oak Avenue",
+      zipCode: "67890",
+    },
+  },
+  {
+    name: "Bob Johnson",
+    age: 35,
+    city: "Yet Another City",
+    isStudent: false,
+    grades: [75, 80, 78],
+    address: {
+      street: "789 Pine Lane",
+      zipCode: "54321",
+    },
+  },
+];
+
+module.exports = members;
